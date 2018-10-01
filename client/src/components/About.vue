daa0475b6e6cfdf3c5ab300cc46ac7b956bf1ff2
Ofir Golan <oig@duke.edu>
2018-09-30 22:33:16 -0400

    added home page title

--- client/src/components/About.vue
+++ client/src/components/About.vue
@@ -1,11 +1,23 @@
 <template>
     <div>
-        hello about
+       
+    <center> <h1 class="md-display-3">Welcome to CostCoach</h1></center>
+    
+        <div>
+            <center> <h2 class ="md-body-2"> Some informational stuff about the app </h2></center>
+        </div>
     </div>
+
+    
+   
+
+
 </template>
+
 
 <script>
 export default {
     name: "About"
 }
 </script>
+
