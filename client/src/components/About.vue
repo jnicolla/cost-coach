09c0df6edaf9bab0ca94cebf9da446ce3ed79628
Ofir Golan <oig@duke.edu>
2018-10-05 00:40:02 -0400

    edited about page

--- client/src/components/About.vue
+++ client/src/components/About.vue
@@ -7,26 +7,25 @@
         <div>
             <center><md-content class="md-primary">  
     
-                
-                    <h1 style="font-size:200%;"> Why CostCoach?<br /></h1>
+                <br />
+                    <h1> Why CostCoach?<br /></h1>
                         
-                            <li style="font-size:200%;">CostCoach is here to help guide you from the first day of your cancer treatment.<br /></li>
-                            <li style="font-size:200%;">The financial burden of cancer can be overwhelming, and it is difficult to plan for the costs when we don't know how much
+                            <li>CostCoach is here to help guide you from the first day of your cancer treatment.<br /></li>
+                            <li>The financial burden of cancer can be overwhelming, and it is difficult to plan for the costs when we don't <br /> know how much
                                  our treatment plan is expected to be.<br /></li>
                         
-                   
-                    <h1 style="font-size:200%;"> Our Goal<br /></h1>
-                         <ul>
-                            <li style="font-size:200%;">If you click on the Calculator tab above, you will be taken to a short questionnaire. Based on the answers you provide,
-                                 we</li>
-                            <li style="font-size:200%;">will calculate the date at which you shoulde expect to reach your out of pocket maximum, as well as what the cost of your visits should be.</li>
-                        </ul>
-                    <li style="font-size:200%;">Fighting The Stigma</li>
-                        <ul>
-                            <li style="font-size:200%;">Having a conversation with your health providers about your finances can also be challening. In the Guide page
-                we provide information to try to help you have this conversation with your medical provider.</li>
+                <br />
+                    <h1> Our Goal<br /></h1>
+                         
+                            <li>If you click on the Calculator tab above, you will be taken to a short questionnaire. <br /></li>
+                            <li>Based on the answers you provide, we will calculate the date at which you shoulde expect to reach your <br /> out of pocket maximum, as well as what the cost of your visits should be.<br /></li>
+                <br />
+                    <h1>Fighting The Stigma<br /></h1>
                         
-                        </ul>
+                            <li>Having a conversation with your health providers about your finances can also be challening.<br /></li> 
+                            <li>In the Guide page we provide information to try to help you have this conversation with your medical provider.<br /></li>
+                        
+                        
                 
             </md-content>
             </center>
@@ -50,7 +49,8 @@
     display: inline-block;
     justify-content: center;
     align-items: center;
-    background-color:lightgrey;
+    font-size:120%;
+    text-align: center;
   }
   
 </style>
