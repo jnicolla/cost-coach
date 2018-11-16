bf7526709f67d2804f31b1883716b987ca0fcb4e
stefani.vukajlovic <stefani.vukajlovic@duke.edu>
2018-11-16 07:08:17 -0500

    comments

--- server/app.py
+++ server/app.py
@@ -10,8 +10,8 @@ CORS(app)
 @app.route('/calculate', methods=['POST'])
 def calculate():
     answers = request.get_json()
-    # res = script_calc(answers.get('insurance'),answers.get('outOfPocket'),answers.get('copay'),answers.get('diagnosis'),answers.get('medications'))
-    res = script_calc('Duke Select', 'Prostate Cancer', 'Oral')
+    res = script_calc(answers.get('insurance'),answers.get('diagnosis'),answers.get('medications'))
+    # res = script_calc('Duke Select', 'Prostate Cancer', 'Oral')
     response = { 'html':res[0], 'cost':res[1]}
     return jsonify(response)
 
@@ -20,7 +20,13 @@ def calculate():
 As you can see below, the data returned should be strings delimited by semi-colons.
 Do as much as you can to clean the data before sending the response
 and we'll collaborate to make it work with the frontend
+
+The example of the output of the processing scrypt:
+format: 'Visit, Catagory, OOPM'
+['1, Lab, 1300', '2, Specialist, 400']
+
 '''
+
 @app.route('/test', methods=['POST'])
 def test():
     data = [
