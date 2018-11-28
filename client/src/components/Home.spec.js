e755f72d9e332e8e8775911df1fd0240d0883973
Abdulla Shahid <as737@duke.edu>
2018-11-28 13:46:36 -0500

    Added More tests for V-Cards

--- client/src/components/Home.spec.js
+++ client/src/components/Home.spec.js
@@ -77,4 +77,43 @@ describe ('Home.vue',()=>
         expect (but_2.text()).toBe('Guide')
         
     })
+    test('Checks to see if text on V-card 1 is correct ', () => 
+    {
+       
+        const wrapper = mount(Home)
+        const box_1 = wrapper.find('#Card_1')
+        expect (box_1.text()).toBe('Welcome to CostCoach '+
+        '\n                          CostCoach is the world\'s first application that can calculate your cancer treatment costs. ' + 
+        '\n                          You only need three pieces of information to use it: your insurance plan, diagnosis, and medications.' + 
+        '\n                          Press the button below and get started Calculator')
+        
+    })
+
+    test('Checks to see if text on V-card 2 is correct ', () => 
+    {
+       
+        const wrapper = mount(Home)
+        const box_2 = wrapper.find('#Card_2')
+        expect (box_2.text()).toBe('Fighting the Stigma '+
+        '\n                          Having a conversation with your health care provider about finances can be challenging. To make ' + 
+        '\n                          this conversation easier, a list of common questions have been assembled in our Guide.' +
+        '\n                          Find your questions here Guide')
+        
+    })
+
+    test('Checks to see if text on V-card 3 is correct ', () => 
+    {
+       
+        const wrapper = mount(Home)
+        const box_3 = wrapper.find('#Card_3')
+        expect (box_3.text()).toBe('Did you know? '+
+        '\n                          The Agency for Healthcare Research and Quality (AHRQ) estimates that the direct medical costs '+
+        '\n                          (total of all health care costs) for cancer in the US in 2015 were ' +
+        '\n                          $80.2 billion.'+
+        '\n                         52 percent of patients desire a cost discussion with their oncologist.'+
+        '\n                         57 percent of patients reported lower treatment costs due to a discussion.'+
+        '\n                         19 percent of patients actually have a cost discussion.'
+        )
+        
+    })
 }) 
