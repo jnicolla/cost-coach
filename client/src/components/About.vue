ee25ef84461c735df80a5daf446e39f6aa7a4f96
CoderCoda <mitchell.li.duke@gmail.com>
2018-12-04 21:47:43 -0500

    ran build

--- client/src/components/About.vue
+++ client/src/components/About.vue
@@ -23,6 +23,7 @@
                     <p>
                       <span class="subheading">The questions in the conversation guide page are pulled from pathlighthealth.com, which belongs to our client.</span>
                     </p>
+                    <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
                   </div>
                 </v-flex>
               </v-layout>
